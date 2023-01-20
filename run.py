eb31321f4fa41c32fb7642f126064e0c71fc3bfb
youssef sassi <56152541+youssef9774@users.noreply.github.com>
2023-01-20 16:12:42 +0100

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ Created on Thu Sep 30 11:42:14 2021
 """
 
 from app import app
-app.run(debug = False, port = 8000, host='0.0.0.0')
+app.run(debug = False, host='0.0.0.0') #port = 8000
 #app.run(debug=True)
+
 
