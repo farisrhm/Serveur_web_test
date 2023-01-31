f620e30ea1b1b3a2fde1442a4d18682d6964e8b7
farisrhm <82390826+farisrhm@users.noreply.github.com>
2023-01-31 12:17:18 +0100

    Update run.py

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ Created on Thu Sep 30 11:42:14 2021
 
 from app import app
 
-app.run(debug = False,host='0.0.0.0', port=8000) #port = 8000
+app.run(debug = False,host='0.0.0.0', port=17441) #port = 8000
 #app.run(debug=True)
 
 
